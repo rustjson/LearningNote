12992eb5772b9a0347f0228b211c2d93c34452b8
Jason Young <shengjie5@staff.weibo.com>
2015-11-17 15:25:53 +0800

    Added ext example for ruby

---
+++ ruby/gems-ext/my_malloc/ext/my_malloc/extconf.rb
@@ -0,0 +1,8 @@
+require "mkmf"
+
+abort "missing malloc()" unless have_func "malloc"
+abort "missing free()" unless have_func "free"
+
+create_makefile "my_malloc"
+#create_makefile "my_malloc/my_malloc"
+
---
+++ ruby/gems-ext/my_malloc/ext/my_malloc/my_malloc.c
@@ -0,0 +1,88 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  my_malloc.c
+ *
+ *    Description: MY_MALLOC 
+ *
+ *        Version:  1.0
+ *        Created:  11/17/2015 02:50:52 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jason YOung
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <ruby.h>
+
+struct my_malloc {
+	size_t size;
+	void *ptr;
+};
+
+static void my_malloc_free(void *p) {
+	struct my_malloc *ptr = p;
+	if (ptr->size > 0) {
+		free(ptr->ptr);
+	}
+}
+
+static VALUE my_malloc_alloc(VALUE klass) {
+	VALUE obj;
+	struct my_malloc *ptr;
+
+	obj = Data_Make_Struct(klass, struct my_malloc, NULL, my_malloc_free, ptr);
+
+	ptr->size = 0;
+	ptr->ptr = NULL;
+	
+	return obj;
+}
+
+static VALUE my_malloc_init(VALUE self, VALUE size) {
+
+	struct my_malloc *ptr;
+	size_t requested = NUM2SIZET(size);
+	
+	if(0 == requested) {
+		rb_raise(rb_eArgError, "unable to allocate 0 bytes");
+	}
+	
+	Data_Get_Struct(self, struct my_malloc, ptr);
+	
+	ptr->ptr = malloc(requested);
+	
+	if (NULL == ptr->ptr)
+		rb_raise(rb_eArgError, "unable to allocate %ld bytes", requested);
+	
+	ptr->size = requested;
+
+	return self;
+}
+
+static VALUE my_malloc_release(VALUE self) {
+	struct my_malloc *ptr;
+
+	Data_Get_Struct(self, struct my_malloc, ptr);
+	
+	if (0 == ptr->size)
+		return self;
+	
+	ptr->size = 0;
+	free(ptr->ptr);
+	
+	return self;
+}
+
+void Init_my_malloc(void) {
+	VALUE cMyMalloc;
+	
+	cMyMalloc = rb_const_get(rb_cObject, rb_intern("MyMalloc"));
+	
+	rb_define_alloc_func(cMyMalloc, my_malloc_alloc);
+	rb_define_method(cMyMalloc, "initialize", my_malloc_init, 1);
+	rb_define_method(cMyMalloc, "free", my_malloc_release, 0);
+}
+
