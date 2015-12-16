f7f0c16ad2eb379379daf5f724049163cdf41d6c
Jason Young <shengjie5@staff.weibo.com>
2015-12-16 12:25:38 +0800

    better way

--- rust/cargo/phrases/src/main.rs
+++ rust/cargo/phrases/src/main.rs
@@ -1,11 +1,13 @@
-extern crate phrases;
+extern crate language;
+
+use language::{english, french, chinese};
 
 fn main() {
-    println!("Hello in English: {}", phrases::english::greetings::hello());
-    println!("Hello in French: {}", phrases::french::greetings::hello());
-    println!("Hello in Chinese: {}", phrases::chinese::greetings::hello());
+    println!("Hello in English: {}", english::greetings::hello());
+    println!("Hello in French: {}", french::greetings::hello());
+    println!("Hello in Chinese: {}", chinese::greetings::hello());
 
-    println!("Goodbye in English: {}", phrases::english::farewells::goodbye());
-    println!("Goodbye in French: {}", phrases::french::farewells::goodbye());
-    println!("Goodbye in Chinese: {}", phrases::chinese::farewells::goodbye());
+    println!("Goodbye in English: {}", english::farewells::goodbye());
+    println!("Goodbye in French: {}", french::farewells::goodbye());
+    println!("Goodbye in Chinese: {}", chinese::farewells::goodbye());
 }
